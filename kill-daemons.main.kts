c6bfe0373652a8e13721d26ba101ab961fde2585
Paul Woitaschek <woitaschek@posteo.de>
2020-10-19 10:42:02 +0200

    Added an option to auto kill older versions.

--- kill-daemons.main.kts
+++ kill-daemons.main.kts
@@ -4,11 +4,34 @@
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.UsageError
 import com.github.ajalt.clikt.output.TermUi
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
 
 class App : CliktCommand() {
 
+  private val autoKill by option("--auto-kill", "-k").flag(default = false)
+
   enum class DaemonType {
     Gradle, Kotlin
+  }
+
+  data class Version(
+    val major: Int,
+    val minor: Int,
+    val patch: Int?
+  ) : Comparable<Version> {
+
+    override fun compareTo(other: Version): Int {
+      return compareValuesBy(this, other, { it.major }, { it.minor }, { it.patch })
+    }
+  }
+
+  private fun String.toVersion(): Version {
+    val numbers = split(".").map { it.toInt() }
+    val major = numbers[0]
+    val minor = numbers[1]
+    val patch = numbers.getOrNull(2)
+    return Version(major, minor, patch)
   }
 
   data class Daemon(
@@ -22,13 +45,9 @@ class App : CliktCommand() {
       .readLines()
       .filter { line -> line.contains("gradle") || line.contains("kotlin") }
       .mapNotNull { line ->
-        if (line.contains("kotlin")) {
-          println(line)
-        }
         val processId = "(.*?) ".toRegex().find(line)!!.groupValues.drop(1).first().toInt()
-        val gradleVersion =
-          "org.gradle.launcher.daemon.bootstrap.GradleDaemon (.*?) ".toRegex()
-            .find(line)?.groupValues?.drop(1)?.firstOrNull()
+        val gradleVersion = "org.gradle.launcher.daemon.bootstrap.GradleDaemon (.*?) ".toRegex()
+          .find(line)?.groupValues?.drop(1)?.firstOrNull()
         if (gradleVersion == null) {
           val kotlinVersion =
             "kotlin-compiler-embeddable-(.*?)\\.jar".toRegex().find(line)?.groupValues?.drop(1)
@@ -43,7 +62,9 @@ class App : CliktCommand() {
         }
       }
       .groupBy { it.type }
-      .mapValues { it.value.sortedByDescending { it.version } }
+      .mapValues { (_, daemons) ->
+        daemons.sortedByDescending { daemon -> daemon.version }
+      }
       .flatMap { it.value }
 
     if (daemons.isEmpty()) {
@@ -51,30 +72,45 @@ class App : CliktCommand() {
       return
     }
 
-    val promptItems = daemons
-      .mapIndexed { index, daemon ->
-        "[${index + 1}]\t${daemon.type}\t${daemon.version}"
-      }
-      .joinToString(separator = "\n")
-
-    TermUi.echo(promptItems)
-
-    val selectedDaemons = TermUi.prompt("Enter the numbers, separated by commas you want to kill") { input ->
-      input.split(",").map { rawNumber ->
-        val index = (rawNumber.toIntOrNull() ?: -1) - 1
-        daemons.getOrElse(index) {
-          throw UsageError("Invalid input")
+    val daemonsToKill = if (autoKill) {
+      daemonsToAutoKill(daemons)
+    } else {
+      val promptItems = daemons
+        .mapIndexed { index, daemon ->
+          "[${index + 1}]\t${daemon.type}\t${daemon.version}"
         }
-      }
-    }!!
-    selectedDaemons.forEach { daemon ->
+        .joinToString(separator = "\n")
+
+      TermUi.echo(promptItems)
+
+      TermUi.prompt("Enter the numbers, separated by commas you want to kill") { input ->
+        input.split(",").map { rawNumber ->
+          val index = (rawNumber.toIntOrNull() ?: -1) - 1
+          daemons.getOrElse(index) {
+            throw UsageError("Invalid input")
+          }
+        }
+      }!!
+    }
+
+    daemonsToKill.forEach { daemon ->
       val command = if (TermUi.isWindows) {
         "taskkill /F /PID ${daemon.processId}"
       } else {
         "kill -9 ${daemon.processId}"
       }
       Runtime.getRuntime().exec(command)
+      TermUi.echo("killed ${daemon.type}\t${daemon.version}")
     }
+  }
+
+  private fun daemonsToAutoKill(daemons: List<Daemon>): List<Daemon> {
+    return daemons.groupBy { it.type }
+      .map { (_, daemons) ->
+        daemons.sortedBy { it.version }
+          .dropLast(1)
+      }
+      .flatten()
   }
 }
 
